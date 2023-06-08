3744a85ea03b2f01e067741901665348d1fa59e5
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-08 10:27:06 -0300

    Creating your components

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { View, Text, Image } from "react-native";
 class App extends Component{
   render(){
     let nome = "Ricardo";
-    let img = "https://sujeitoprogramador.com/steve.png";
     return (
       <View>
         <Text>Olá Mundo!</Text>
@@ -12,11 +11,23 @@ class App extends Component{
         <Text style= {{ color: "red", fontSize: 25, margin: 15}}>
           Teste
         </Text>
-        <Image
-        source={{ uri: img }}
-        style={{ width: 300, height: 300}}
-        />
         <Text style={{ fontSize: 30}}>{nome}</Text>
+        <Jobs largura={500} altura={200}/>
+      </View>
+    )
+  }
+}
+
+class Jobs extends Component{
+  render(){
+    let img = "https://sujeitoprogramador.com/steve.png";
+    return(
+      <View>
+        <Image
+          source={{ uri: img }}
+          style={{ width: this.props.largura, height: this.props.altura}}
+        />
+      <Text>Steve Jobs</Text>  
       </View>
     )
   }
