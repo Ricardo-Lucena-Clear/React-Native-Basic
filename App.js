1aa571214aa5e76d747b3237cb593e2611e0e540
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-12 10:26:48 -0300

    Slider

--- App.js
+++ App.js
@@ -1,61 +1,42 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text} from 'react-native';
+import { 
+  View,
+  StyleSheet, 
+  Text} from 'react-native';
 
-import {Picker} from '@react-native-picker/picker';
+  import Slider from '@react-native-community/slider';
 
-export default class App extends Component {
-
+class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      pizza: 0,
-      pizzas: [
-        {key: 1, nome: 'Strogonoff', valor: 35.90},
-        {key: 2, nome: 'Calabresa', valor: 59},
-        {key: 3, nome: 'Quatro queijos', valor: 37},
-        {key: 4, nome: 'Brigadeiro', valor: 25.70},
-        {key: 5, nome: 'Portuguesa', valor: 70},
-      ]
+      valor: 0
     };
-  };
+    }
+render(){
+  return(
+    <View style = {styles.container}>
+    <Slider
+      minimumValue={0}
+      maximumValue={100}
+      onValueChange={(valorSelecionado)=>this.setState({valor: valorSelecionado})}
+      value={this.state.valor}
+      minimumTrackTintColor='green'
+      maximumTrackTintColor='red'
+    />
 
- render(){
-
-  let pizzasItem = this.state.pizzas.map( (v, k) => {
-    return <Picker.Item key={k} value={k} label={v.nome} />
-  } )
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.logo}>Menu Pizza</Text>
-
-      <Picker
-      selectedValue={this.state.pizza}
-      onValueChange={ (itemValue, itemIndex) =>  this.setState({pizza: itemValue}) }
-      >
-       {pizzasItem}
-      </Picker>
-
-      <Text style={styles.pizzas}>Voce escolheu: {this.state.pizzas[this.state.pizza].nome}</Text>
-      <Text style={styles.pizzas}>R$: {this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
+    <Text style={{textAlign: 'center', fontSize: 30}}>
+      {this.state.valor.toFixed(0)}
+    </Text>
     </View>
-   );
- }
+    )
+  }
 }
-
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    marginTop: 20,
-  },
-  logo:{
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: 'bold'
-  },
-  pizzas:{
-    marginTop: 15,
-    fontSize: 25,
-    textAlign: 'center'
+    marginTop:150
   }
-});
+})
+
+export default App;
