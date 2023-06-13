6a0581235b8b8c22cdcf82911d3514b20186d943
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-13 09:31:58 -0300

    Movies App

--- App.js
+++ App.js
@@ -1,34 +1,48 @@
-import React, {Component} from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import React, { Component } from 'react';
+import { View, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 
-import api from "./src/services/api";
-import Filmes from "./src/Filmes";
+import api from './src/services/api';
+import Filmes from './src/Filmes';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      filmes: []
-    }
+      filmes: [],
+      loading: true
+    };
   }
 
   async componentDidMount(){
     const response = await api.get('r-api/?api=filmes');
     this.setState({
-      filmes: response.data
-    })
+      filmes: response.data,
+      loading: false
+    });
   }
-  render (){
-    return(
-      <View style={styles.container}>
-        <FlatList
-        data={this.state.filmes}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({item}) => <Filmes data={item}/>}
-        />
-      </View>
-    )
+
+  render() {
+
+    if(this.state.loading){
+      return(
+        <View style={{alignItems: 'center', justifyContent: 'center', flex:1}}>
+          <ActivityIndicator color="#09A6FF" size={40}/>
+        </View>
+      )
+    }else{
+      return(
+        <View style={styles.container}>
+  
+          <FlatList
+          data={this.state.filmes}
+          keyExtractor={item => item.id.toString() }
+          renderItem={ ({item}) => <Filmes data={item} /> }
+          />
+  
+        </View>
+      )
+    }
   }
 }
 
@@ -36,6 +50,7 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
   }
-})
+});
 
 export default App;
+
